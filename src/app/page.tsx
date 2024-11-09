d954ac34196c01923f3cb7d096bdf608d3cd645e
imuskanbatool7 <162132449+imuskanbatool7@users.noreply.github.com>
2024-11-09 18:36:24 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+
+
+import Header from "@/components/Header";
+import Image from "next/image";
+
+
+
+export default function Home() {
+  return (<>
+  <div>
+     <Header/>
+  </div>
+  </>
+     
+  );
+}
+
